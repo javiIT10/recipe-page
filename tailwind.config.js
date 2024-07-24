d9637e03ebd1104ae596fb8a269f4380689416f1
Javier <javier.duarte.iti@gmail.com>
2024-07-24 07:33:42 -0500

    Añadiendo Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        "young-serif": ["Young Serif", "sans-serif"],
+      },
+      colors: {
+        /* ## Colors */
+        white: "hsl(0, 0%, 100%)",
+
+        "stone-100": "hsl(30, 54%, 90%)",
+        "stone-150": "hsl(30, 18%, 87%)",
+        "stone-600": "hsl(30, 10%, 34%)",
+        "stone-900": "hsl(24, 5%, 18%)",
+
+        "brown-800": "hsl(14, 45%, 36%)",
+
+        "rose-800": "hsl(332, 51%, 32%)",
+        "rose-50": "hsl(330, 100%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
